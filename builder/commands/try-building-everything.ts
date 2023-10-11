d22ce708b90d5dd1878fb872b9b4a3c054f4bc9d
Dulguun Otgon <dulguun0225@gmail.com>
2023-10-11 17:23:21 +0800

    fix

--- builder/commands/try-building-everything.ts
+++ builder/commands/try-building-everything.ts
@@ -3,24 +3,24 @@ import path from 'path';
 import build from '../src/build';
 
 async function main() {
+
     const packagesPath = path.resolve(__dirname, '..', '..', 'packages');
     const folderNames = fs.readdirSync(packagesPath, { withFileTypes: true }).filter(dirent => {
-        return dirent.isDirectory() && (['core', 'gateway', 'workers'].includes(dirent.name) || /^plugin-.+?-api$/.test(dirent.name));
+        return dirent.name !== 'plugin-integrations-api' && dirent.isDirectory() && (['core', 'gateway', 'workers', 'crons'].includes(dirent.name) || /^plugin-.+?-api$/.test(dirent.name));
     }).map(dirent => dirent.name);
 
-    
-    for(const folderName of folderNames) {
+
+    for (const folderName of folderNames) {
         try {
             console.log(`----------------- Building ${folderName} --------------------------`);
             await build(folderName);
-            console.log(`----------------- Finished building ${folderName} --------------------------\n\n\n`);
         } catch (e) {
-            break;
         }
-        
+
+        console.log(`----------------- Finished building ${folderName} --------------------------\n\n\n`);
+
     }
-    // await build('gateway');
-    
+
 }
 
 main();
